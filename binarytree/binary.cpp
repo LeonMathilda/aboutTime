4fed63a6b649bbddd5a74a7ed7e5f75c9f25ea50
LeonMathilda <506856123@qq.com>
2014-03-07 13:30:17 -0800

    dfa

---
+++ binarytree/binary.cpp
@@ -0,0 +1,9 @@
+//
+//  binary.cpp
+//  binarytree
+//
+//  Created by 李昂 on 14-3-7.
+//  Copyright (c) 2014年 李昂. All rights reserved.
+//
+
+#include "binary.h"
---
+++ binarytree/binary.h
@@ -0,0 +1,14 @@
+//
+//  binary.h
+//  binarytree
+//
+//  Created by 李昂 on 14-3-7.
+//  Copyright (c) 2014年 李昂. All rights reserved.
+//
+
+#ifndef __binarytree__binary__
+#define __binarytree__binary__
+
+#include <iostream>
+
+#endif /* defined(__binarytree__binary__) */

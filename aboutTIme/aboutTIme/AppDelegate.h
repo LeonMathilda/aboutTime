bd28c5fc433c84b6235a52a7873306ed75bcf626
LeonMathilda <506856123@qq.com>
2014-03-07 12:42:51 -0800

    Initial Commit

---
+++ aboutTIme/aboutTIme/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  aboutTIme
+//
+//  Created by 李昂 on 14-3-7.
+//  Copyright (c) 2014年 李昂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
